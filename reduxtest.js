fbca933e53c7b04ce8451859b0cd2b8460362920
Prod <system-67@Prods-MacBook-Air.local>
2021-06-13 20:19:36 +0530

    added adding/substracting random age

--- reduxtest.js
+++ reduxtest.js
@@ -8,8 +8,16 @@ const myReducer = (state = initalState, action) => {
     switch (action.type) {
         case 'ADD':
             return ({ ...state, age: ++state.age });
+
         case 'SUB':
             return ({ ...state, age: --state.age });
+
+        case 'ADD_RANDOM':
+            return ({ ...state, age: state.age + action.payload.age });
+
+        case 'SUB_RANDOM':
+            return ({ ...state, age: state.age - action.payload.age });
+
         default:
             return state;
     }
@@ -23,3 +31,5 @@ store.subscribe(() => {
 
 store.dispatch({ type: 'ADD' });
 store.dispatch({ type: 'SUB' });
+store.dispatch({ type: 'ADD_RANDOM', payload: { age: 10 } });
+store.dispatch({ type: 'SUB_RANDOM', payload: { age: 5 } });
